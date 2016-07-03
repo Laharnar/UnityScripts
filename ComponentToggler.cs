a2ce70422ae6ca7b4e283c6fcd81b5e5495f4efe
Laharnar <marko.laharnar@gmail.com>
2016-07-03 14:01:35 +0200

    Update ComponentToggler.cs

--- ComponentToggler.cs
+++ ComponentToggler.cs
@@ -1,6 +1,72 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+// version 3, automaticaly maps in queue by -1, and allows custom maps
+public class TransformToggler : MonoBehaviour {
+
+    public ComponentTarget[] targets;
+    private int _activePlayer = -1;
+
+    public int ActivePlayer
+    {
+        get { return _activePlayer; }
+        set
+        {
+            if (targets.Length == 0)
+            {
+                return;
+            }
+            _activePlayer = value % targets.Length;
+        }
+    }
+
+    public void NextTarget()
+    {
+        if (targets.Length == 0)
+	    {
+		     return;
+	    }
+
+        ActivePlayer++;
+        if (ActivePlayer-1 > -1)
+	    {
+            targets[ActivePlayer-1].target.SetActive(false);
+        }
+        else
+        {
+            targets[targets.Length - 1].target.SetActive(false);
+        }
+        targets[ActivePlayer].target.SetActive(true);
+    }
+
+    void Start()
+    {
+        for (int i = 0; i < targets.Length; i++)
+        {
+            if (targets[i].nextTarget == -1)
+	        {
+                targets[i].nextTarget = (i + 1) % targets.Length;
+            }
+            else
+            {
+                targets[i].nextTarget = targets[i].nextTarget % (targets.Length);
+            }
+        }
+
+        NextTarget();
+    }
+
+}
+
+[System.Serializable]
+public class ComponentTarget
+{
+    public GameObject target;
+    public string commentary;
+    public int nextTarget = -1; // -1 means next target in queue
+}
+
+
 // version 2
 public class TransformToggler : MonoBehaviour {
 
