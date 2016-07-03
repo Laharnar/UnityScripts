f556b432978e498602ef0cdca1246a2709211c89
Laharnar <marko.laharnar@gmail.com>
2016-07-03 13:08:47 +0200

    Add files via upload
    Base component for MonoBehaviourToggling

---
+++ ComponentToggler.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class ComponentToggler {
+
+    MonoBehaviour[] targets;
+    private int _activePlayer = -1;
+
+    public int ActivePlayer
+    {
+        get { return _activePlayer; }
+        set
+        {
+            if (targets.Length == 0)
+            {
+                return;
+            }
+            _activePlayer = value % targets.Length;
+        }
+    }
+
+    public void NextTarget()
+    {
+        ActivePlayer++;
+        for (int i = 0; i < targets.Length; i++)
+        {
+            targets[i].enabled = false;
+        }
+        targets[ActivePlayer].enabled = true;
+    }
+
+    
+}
