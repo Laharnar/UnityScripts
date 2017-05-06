e4a4f11b6c60dacdb03b660192f70116ed7cb7af
Marko Laharnar <marko.laharnar@gmail.com>
2017-05-06 19:07:41 +0200

    Add files via upload

---
+++ UtilityTrees/AiGraph.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using System.Collections.Generic;
+
+/// <summary>
+/// Displays actions and scores on selected ai unit.
+/// </summary>
+public class AiGraph : EditorWindow{
+
+    [MenuItem("Examples/GUILayout TextField")]
+    static void Init() {
+        EditorWindow window = GetWindow(typeof(AiGrahp));
+        window.Show();
+    }
+
+    void OnGUI() {
+        GUILayout.Label("Select an object in the hierarchy view");
+        if (Selection.activeGameObject) {
+            Selection.activeGameObject.name =
+                EditorGUILayout.TextField("Object Name: ", Selection.activeGameObject.name);
+            if (!Application.isPlaying) {
+
+                EditorGUILayout.LabelField("Press play...");
+            } else {
+
+                AssaultCraft aiTarget = Selection.activeGameObject.GetComponent<AssaultCraft>();
+                if (aiTarget) {
+                    // display ai scores
+                    List<UtilityNode> ut = aiTarget.root.choices;
+                    for (int i = 0; i < ut.Count; i++) {
+                        EditorGUILayout.FloatField(ut[i].method.Method.Name, ut[i].sum);
+                    }
+                }
+            }
+        }
+        this.Repaint();
+    }
+}
